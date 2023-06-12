4b09e690bdd39d3e00b64845157d150a3602630a
bipin67678grg@gmail.com <bipin67678grg@gmail.com>
2023-06-12 14:53:00 +0545

    basic components

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Apply from '../components/Apply/Apply'
+import './home.scss'
+const Home = () => {
+    return (
+        <div className="home">
+            <Apply />
+        </div>
+    )
+}
+
+export default Home
